1997f78b657417196ddd8d5a62e26f8ba5daf8b4
Mercury <quocan.omg@gmail.com>
2020-10-19 19:48:03 +0700

    Area2_Overworld dòng 2

--- BlasterMaster_Area2_final.tsx
+++ BlasterMaster_Area2_final.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.2" name="BlasterMaster_Area2" tilewidth="16" tileheight="16" spacing="4" margin="1" tilecount="143" columns="11">
- <image source="G:\Code\Game\NMGame_blaster_master\BlasterMaster_Area2.png" width="218" height="258"/>
+ <image source="D:\Learning\3rdYear\Game\NMGame_blaster_master\BlasterMaster_Area2.png" width="218" height="258"/>
 </tileset>
