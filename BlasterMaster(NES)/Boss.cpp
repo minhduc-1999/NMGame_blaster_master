82dbc30ee88092d47de698d4a89edc323c125309
LaggeR <vo.binh224@gmail.com>
2021-01-12 15:29:07 +0700

    add boss at section33 ovw, edit boss

--- BlasterMaster(NES)/Boss.cpp
+++ BlasterMaster(NES)/Boss.cpp
@@ -7,55 +7,73 @@ Boss::Boss(float x, float y, int hand_ani_set_id, int arm_ani_set_id) :CDynamicG
 	LPANIMATION_SET ani_set = animation_sets->Get(hand_ani_set_id);
 	LPANIMATION_SET ani_set2 = animation_sets->Get(arm_ani_set_id);
 	handLeft5 = new Hand(x, y, 5,true);
-	handLeft1 = new Hand(x, y, 4, true);
-	handLeft2 = new Hand(x, y, 3, true);
-	handLeft3 = new Hand(x, y, 2,true);
-	handLeft4 = new Hand(x, y, 1,true);
+	handLeft4 = new Hand(x, y, 4, true);
+	handLeft3 = new Hand(x, y, 3, true);
+	handLeft2 = new Hand(x, y, 2,true);
+	handLeft1 = new Hand(x, y, 1,true);
 	handLeft5->SetAnimationSet(ani_set);
 	handLeft1->SetAnimationSet(ani_set2);
 	handLeft2->SetAnimationSet(ani_set2);
 	handLeft3->SetAnimationSet(ani_set2);
 	handLeft4->SetAnimationSet(ani_set2);
-	handLeft5->SetState(HAND_STATE_FLYING_DOWN);
-	handLeft2->SetState(HAND_STATE_FLYING_DOWN);
-	handLeft3->SetState(HAND_STATE_FLYING_DOWN);
-	handLeft4->SetState(HAND_STATE_FLYING_DOWN);
-	handLeft1->SetState(HAND_STATE_FLYING_DOWN);
+	handLeft5->SetType(41);
+	handLeft4->SetType(41);
+	handLeft3->SetType(41);
+	handLeft2->SetType(41);
+	handLeft1->SetType(41);
 	handRight5 = new Hand(x, y, 5, false);
-	handRight1 = new Hand(x, y, 4, false);
-	handRight2 = new Hand(x, y, 3, false);
-	handRight3 = new Hand(x, y, 2, false);
-	handRight4 = new Hand(x, y, 1, false);
+	handRight4 = new Hand(x, y, 4, false);
+	handRight3 = new Hand(x, y, 3, false);
+	handRight2 = new Hand(x, y, 2, false);
+	handRight1 = new Hand(x, y, 1, false);
 	handRight5->SetAnimationSet(ani_set);
 	handRight1->SetAnimationSet(ani_set2);
 	handRight2->SetAnimationSet(ani_set2);
 	handRight3->SetAnimationSet(ani_set2);
 	handRight4->SetAnimationSet(ani_set2);
-	handRight5->SetState(HAND_STATE_FLYING_DOWN);
-	handRight2->SetState(HAND_STATE_FLYING_DOWN);
-	handRight3->SetState(HAND_STATE_FLYING_DOWN);
-	handRight4->SetState(HAND_STATE_FLYING_DOWN);
-	handRight1->SetState(HAND_STATE_FLYING_DOWN);
-	//handRight = new Hand(x + 20, y, false, arm_ani_set_id);
-	//handRight->SetAnimationSet(ani_set);
-	//handRight->SetState(HAND_STATE_FLYING);
+	handRight5->SetType(41);
+	handRight4->SetType(41);
+	handRight3->SetType(41);
+	handRight2->SetType(41);
+	handRight1->SetType(41);
 	nx = -1;
 	vy = 0;
 	vx = 0;
-	lastTime = GetTickCount();
+
+	detroyTime = 0;
 }
 
 int Boss::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 {
+	if (state == BOSS_STATE_DIE)
+	{
+		isUpdated = true;
+		isRendered = false;
+		vector<LPDYNAMICOBJECT> hands = CreateHands();
+		for (int i = 0; i < hands.size(); i++)
+		{
+			hands[i]->SetState(HAND_STATE_DIE);
+		}
+
+		handLeft5->Update(dt, coObjects, GetPosition(), 0);
+		int temp = handLeft5->GetNextPointIndex();
+		handLeft1->Update(dt, coObjects, GetPosition(), temp);
+		handLeft2->Update(dt, coObjects, GetPosition(), temp);
+		handLeft3->Update(dt, coObjects, GetPosition(), temp);
+		handLeft4->Update(dt, coObjects, GetPosition(), temp);
+
+		handRight5->Update(dt, coObjects, GetPosition(), 0);
+		int temp2 = handRight5->GetNextPointIndex();
+		handRight1->Update(dt, coObjects, GetPosition(), temp2);
+		handRight2->Update(dt, coObjects, GetPosition(), temp2);
+		handRight3->Update(dt, coObjects, GetPosition(), temp2);
+		handRight4->Update(dt, coObjects, GetPosition(), temp2);
+
+		return 0;
+	}
+
 	if (isUpdated)
 		return -1;
-	DWORD now = GetTickCount();
-	if (now - lastTime >= 3000)
-	{
-		handLeft5->SetState(HAND_STATE_FLYING_BIAS);
-		handRight5->SetState(HAND_STATE_FLYING_BIAS);
-		lastTime = now;
-	}
 	CDynamicGameObject::Update(dt);
 
 	vector<LPCOLLISIONEVENT> coEvents;
@@ -84,19 +102,34 @@ int Boss::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 		for (UINT i = 0; i < coEventsResult.size(); i++)
 		{
 			LPCOLLISIONEVENT e = coEventsResult[i];
-			// if e->obj is Gate 
-			
-		}
-
-		if (ntx != 0)
-		{
-			vx = -vx;
-			nx = -nx;
-		}
-
-		if (nty != 0)
-		{
-			vy = -vy;
+			int objType = e->obj->GetType();
+			if (objType != 15 && objType != 17)
+			{
+				if (e->nx != 0)
+				{
+					x += (1 - e->t) * dx - e->nx * 0.4f;
+				}
+				else
+				{
+					y += (1 - e->t) * dy - e->ny * 0.4f;
+				}
+			}
+			switch (objType)
+			{
+			case 15: case 17:
+				if (e->nx != 0) 
+				{
+					vx = -vx;
+					nx = -nx;
+				}
+				if (e->ny != 0)
+				{
+					vy = -vy;
+				}
+				break;
+			default:
+				break;
+			}
 		}
 
 		//TODO: Collision logic with dynamic object (bots)
@@ -105,26 +138,19 @@ int Boss::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 	// clean up collision events
 	for (UINT i = 0; i < coEvents.size(); i++) delete coEvents[i];
 
-	handLeft5->Update(dt, coObjects, GetPosition());
-	handLeft1->SetState(handLeft5->GetState());
-	handLeft2->SetState(handLeft5->GetState());
-	handLeft3->SetState(handLeft5->GetState());
-	handLeft4->SetState(handLeft5->GetState());
-	handLeft1->Update(dt, coObjects, GetPosition());
-	handLeft2->Update(dt, coObjects, GetPosition());
-	handLeft3->Update(dt, coObjects, GetPosition());
-	handLeft4->Update(dt, coObjects, GetPosition());
+	handLeft5->Update(dt, coObjects, GetPosition(), 0);
+	int temp = handLeft5->GetNextPointIndex();
+	handLeft1->Update(dt, coObjects, GetPosition(), temp);
+	handLeft2->Update(dt, coObjects, GetPosition(), temp);
+	handLeft3->Update(dt, coObjects, GetPosition(), temp);
+	handLeft4->Update(dt, coObjects, GetPosition(), temp);
 
-	handRight5->Update(dt, coObjects, GetPosition());
-	handRight1->SetState(handRight5->GetState());
-	handRight2->SetState(handRight5->GetState());
-	handRight3->SetState(handRight5->GetState());
-	handRight4->SetState(handRight5->GetState());
-	handRight1->Update(dt, coObjects, GetPosition());
-	handRight2->Update(dt, coObjects, GetPosition());
-	handRight3->Update(dt, coObjects, GetPosition());
-	handRight4->Update(dt, coObjects, GetPosition());
-	//handRight->Update(dt, coObjects, GetPosition());
+	handRight5->Update(dt, coObjects, GetPosition(), 0);
+	int temp2 = handRight5->GetNextPointIndex();
+	handRight1->Update(dt, coObjects, GetPosition(), temp2);
+	handRight2->Update(dt, coObjects, GetPosition(), temp2);
+	handRight3->Update(dt, coObjects, GetPosition(), temp2);
+	handRight4->Update(dt, coObjects, GetPosition(), temp2);
 
 
 	isUpdated = true;
@@ -137,23 +163,52 @@ void Boss::Render()
 	if (isRendered)
 		return;
 	int ani = BOSS_ANI_FLYING;
+	if (state == BOSS_STATE_DIE)
+	{
+		animation_set->at(ani)->RenderFrame(0,x, y, nx, 253);
+		if (GetTickCount64() - detroyTime >= 5000)
+		{
+			isDestroyed = true;
+		}
+	}
+	else
+	{
+		animation_set->at(ani)->Render(x, y, nx);
+	}
 
-	animation_set->at(ani)->Render(x, y, nx);
-
-	handLeft4->Render();
-	handLeft3->Render();
-	handLeft2->Render();
 	handLeft1->Render();
+	handLeft2->Render();
+	handLeft3->Render();
+	handLeft4->Render();
 	handLeft5->Render();
 
-	handRight4->Render();
-	handRight3->Render();
-	handRight2->Render();
 	handRight1->Render();
+	handRight2->Render();
+	handRight3->Render();
+	handRight4->Render();
 	handRight5->Render();
 
 	isRendered = true;
 	isUpdated = false;
+}
+
+vector<LPDYNAMICOBJECT> Boss::CreateHands()
+{
+	vector<LPDYNAMICOBJECT> hands;
+	
+	hands.push_back(handRight1);
+	hands.push_back(handRight2);
+	hands.push_back(handRight3);
+	hands.push_back(handRight4);
+	
+	hands.push_back(handLeft1);
+	hands.push_back(handLeft2);
+	hands.push_back(handLeft3);
+	hands.push_back(handLeft4);
+
+	hands.push_back(handRight5);
+	hands.push_back(handLeft5);
+	return hands;
 }
 
 void Boss::SetState(int state)
@@ -164,6 +219,11 @@ void Boss::SetState(int state)
 	case BOSS_STATE_FLYING:
 		vy = BOSS_FLYING_SPEED_Y;
 		vx = -BOSS_FLYING_SPEED_X;
+		break;
+	case BOSS_STATE_DIE:
+		vy = 0;
+		vx = 0;
+		detroyTime = GetTickCount64();
 		break;
 	}
 }
--- BlasterMaster(NES)/Hand.h
+++ BlasterMaster(NES)/Hand.h
@@ -9,26 +9,31 @@
 #define HAND_FLYING_SPEED_X           0.04f
 #define HAND_FLYING_SPEED_Y           0.04f
 
-#define HAND_STATE_FLYING_LEFT           0
-#define HAND_STATE_FLYING_UP             1
-#define HAND_STATE_FLYING_RIGHT          2
-#define HAND_STATE_FLYING_DOWN           3
-#define HAND_STATE_FLYING_BIAS           4
+#define HAND_STATE_FLYING       0
+#define HAND_STATE_DIE          1
 
 #define HAND_ANI               0
 
 class Hand : public CDynamicGameObject
 {
+    D3DXVECTOR2 nextPosition;
+    D3DXVECTOR2 currentPosition;
+    int currentPointIndex;
+    int nextPointIndex;
     D3DXVECTOR2 handPosition, bossPosition;
     int nx;
     int ny;
     int index;
     bool isLeftHand;
     DWORD lastTime = 0;
+    DWORD detroyTime = 0;
     float leftSide, topSide, rightSide, bottomSide;
+    float CalcD(D3DXVECTOR2 a, D3DXVECTOR2 b);
 public:
     Hand(float x, float y,int _index, bool _isLeftHand);
     void SetState(int state);
-    int Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects, D3DXVECTOR2 bossPosition);
+    int Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects, D3DXVECTOR2 bossPosition, int nextPointIndex);
+    //int Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects, D3DXVECTOR2 bossPosition,int beforeNodeNextPosIndex);
     void Render();
+    int GetNextPointIndex() { return nextPointIndex; }
 };
